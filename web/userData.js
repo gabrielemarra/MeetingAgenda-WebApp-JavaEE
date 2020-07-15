ba130782a03e7c85b7eb28cd5239ab9937acea25
Alessandro Lisi <10621058@polimi.it>
2020-07-15 12:06:01 +0200

    user info now printed in home page

--- web/userData.js
+++ web/userData.js
@@ -5,7 +5,7 @@ window.addEventListener("load", () => {
                 var message = req.responseText;
                 if (req.status == 200) {
                     var user = JSON.parse(message);
-                    document.getElementById("id_userDisName").innerText = user.displayedName;
+                    document.getElementById("id_userDispName").innerText = "Welcome " + user.displayedName;
                     document.getElementById("id_userEmail").innerText = user.email;
 
                 }
