5996387ff4888b3f97db89ba7f88d128e98c0390
Alessandro Lisi <10621058@polimi.it>
2020-07-17 18:11:10 +0200

    banane

--- web/scripts/meetingCreation.js
+++ web/scripts/meetingCreation.js
@@ -1,4 +1,65 @@
+
+
+
 (function() { // avoid variables ending up in the global scope
+
+    function openModal() {
+        $("#invitationModal").modal("show");
+    }
+
+    function InvitationList(_alert, _listcontainer) {
+
+        this.alert = _alert;
+        this.listcontainer = _listcontainer;
+
+        this.reset = function () {
+            this.listcontainer.style.visibility = "hidden";
+        };
+
+        this.show = function () {
+            let self = this;
+            makeCall("GET", "../GetInvitationList", null,
+                function (req) {
+                    if (req.readyState == 4) {
+                        var message = req.responseText;
+                        if (req.status == 200) {
+                            var users = JSON.parse(req.responseText);
+                            if (users.length == 0) {
+                                self.alert.textContent += "no user available";
+                                return;
+                            }
+                            self.update(users); // self visible by closure
+
+                        }
+                    } else {
+                        self.alert.textContent = message;
+                    }
+                }
+            );
+
+            this.update = function (invitationList) {
+
+                var row, cell;
+                this.listcontainer.innerHTML = "";
+                // build updated list
+                var self = this;
+
+                var template = document.querySelector("#id_invite_user");
+
+                //print a table row for each meeting
+                invitationList.forEach(function (user) { // self visible here, not this
+                    var row = template.content.cloneNode(true);
+                    var label = row.querySelector("label");
+                    label.textContent = user.displayedName;
+                    label.setAttribute("for", user.userID);
+
+                    self.listcontainer.appendChild(row);
+                });
+                this.listcontainer.style.visibility = "visible";
+            }
+        };
+
+    }
 
     document.getElementById("creationMeetingSubmit").addEventListener('click', (e) => {
         var target=e.target;
@@ -11,16 +72,21 @@
                         switch (req.status) {
                             case 200:
                                 //open modal
-                                alert("APRI MODAL")
+                                openModal();
+                                invitationList = new InvitationList(
+                                    document.getElementById("id_modal_alert"),
+                                    document.getElementById("id_invitation_list"));
+                                invitationList.reset();
+                                invitationList.show();
                                 break;
                             case 400: // bad request
-                                document.getElementById("errormessage").textContent = message;
+                               // document.getElementById("errormessage").textContent = message;
                                 break;
                             case 401: // unauthorized
-                                document.getElementById("errormessage").textContent = message;
+                               // document.getElementById("errormessage").textContent = message;
                                 break;
                             case 500: // server error
-                                document.getElementById("errormessage").textContent = message;
+                                //document.getElementById("errormessage").textContent = message;
                                 break;
                         }
                     }
