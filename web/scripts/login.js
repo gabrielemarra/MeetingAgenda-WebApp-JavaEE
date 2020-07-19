da5f40b9d0be4a97b1faa4ca486d6ef0b3298eb5
gabrielemarra <61741485+gabrielemarra@users.noreply.github.com>
2020-07-19 23:00:06 +0200

    added alert to loginPage

--- web/scripts/login.js
+++ web/scripts/login.js
@@ -6,6 +6,7 @@ function showPasswordAlert(msg) {
     document.getElementById("id_signup_error_message").style.display = "block";
     document.getElementById("id_signup_error_message").textContent = msg;
 }
+
 function pwMismatch() {
 
     let pw = document.getElementById("id_signup_password_input").value;
@@ -17,24 +18,39 @@ function pwMismatch() {
 
     // If confirm password not entered
     else if (pw2 == '')
-        showPasswordAlert ("Please enter confirm password");
+        showPasswordAlert("Please enter confirm password");
 
     // If Not same return False.
     else if (pw != pw2) {
-        showPasswordAlert ("Password did not match: Please try again...")
+        showPasswordAlert("Password did not match: Please try again...")
         return false;
-    }
-
-    else if(pw.length < 6) {
-        showPasswordAlert ("Password is too short, at least 6 characters.")
+    } else if (pw.length < 6) {
+        showPasswordAlert("Password is too short, at least 6 characters.")
         return false;
     }
 
     // If same return True.
-    else{
+    else {
         return true;
     }
 
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+if (urlParams.has('errorMessage')) {
+    const errorParams = urlParams.get('errorMessage');
+    let errorAlert = document.getElementById("id_main_error_message");
+    errorAlert.textContent = errorParams;
+    errorAlert.style.display="block";
+    
+    document.getElementById("id_main_success_message").style.display="none";
+} else if (urlParams.has('successMessage')) {
+    const successParams = urlParams.get('successMessage');
+    let successAlert = document.getElementById("id_main_success_message");
+    successAlert.textContent = successParams;
+    successAlert.style.display="block";
+
+    document.getElementById("id_main_error_message").style.display="none";
 }
 
 document.getElementById('id_signup_password_2_input').onchange = pwMismatchRealtime;
@@ -47,58 +63,55 @@ function pwMismatchRealtime(e) {
         document.getElementById("id_signup_error_message").style.display = "block";
         document.getElementById("id_signup_error_message").textContent = "passwords don't match"
 
-    }
-   else if(pw.length < 6) {
+    } else if (pw.length < 6) {
         document.getElementById("id_signup_error_message").style.display = "block";
         document.getElementById("id_signup_error_message").textContent = "Password is too short, at least 6 characters.";
-    }
-
-    else {
+    } else {
         document.getElementById("id_signup_error_message").style.display = "none";
         document.getElementById("id_signup_error_message").textContent = "";
     }
 }
 
-(function() { // avoid variables ending up in the global scope
+(function () { // avoid variables ending up in the global scope
 
 
     document.getElementById("id_login_button").addEventListener('click', (e) => {
 
-            var form = e.target.closest("form");
-            if (form.checkValidity()) {
-                makeCall("POST", 'CheckLogin', e.target.closest("form"),
-                    function (req) {
-                        if (req.readyState == XMLHttpRequest.DONE) {
-                            var message = req.responseText;
-                            switch (req.status) {
-                                case 200:
-                                    sessionStorage.setItem('username', message);
-                                    window.location.href = "./WebApp";
-                                    break;
-                                case 400: // bad request
-                                    document.getElementById("id_error_message").textContent = message;
-                                    document.getElementById("id_error_message").style.display = "block";
-                                    break;
-                                case 401: // unauthorized
-                                    document.getElementById("id_error_message").textContent = message;
-                                    document.getElementById("id_error_message").style.display = "block";
-                                    break;
-                                case 500: // server error
-                                    document.getElementById("id_error_message").textContent = message;
-                                    document.getElementById("id_error_message").style.display = "block";
-                                    break;
-                            }
+        var form = e.target.closest("form");
+        if (form.checkValidity()) {
+            makeCall("POST", 'CheckLogin', e.target.closest("form"),
+                function (req) {
+                    if (req.readyState == XMLHttpRequest.DONE) {
+                        var message = req.responseText;
+                        switch (req.status) {
+                            case 200:
+                                sessionStorage.setItem('username', message);
+                                window.location.href = "./WebApp";
+                                break;
+                            case 400: // bad request
+                                document.getElementById("id_error_message").textContent = message;
+                                document.getElementById("id_error_message").style.display = "block";
+                                break;
+                            case 401: // unauthorized
+                                document.getElementById("id_error_message").textContent = message;
+                                document.getElementById("id_error_message").style.display = "block";
+                                break;
+                            case 500: // server error
+                                document.getElementById("id_error_message").textContent = message;
+                                document.getElementById("id_error_message").style.display = "block";
+                                break;
                         }
                     }
-                );
-            } else {
-                form.reportValidity();
-            }
+                }
+            );
+        } else {
+            form.reportValidity();
+        }
 
     });
 
     document.getElementById("id_signup_login_button").addEventListener('click', (e) => {
-        if(pwMismatch()) {
+        if (pwMismatch()) {
             var form = e.target.closest("form");
             if (form.checkValidity()) {
                 makeCall("POST", 'SignUp', e.target.closest("form"),
